29f06aac24009a70a61532d025ee037e21197137
Luís Brandão Teixeira <luis.brandao@dtidigital.com.br>
2021-05-13 22:53:47 -0300

    #S2004 - Aparecer os cards de maneira funcional de acordo com a regra de horas

--- cogym/lib/Views/MainPage/mainPage.dart
+++ cogym/lib/Views/MainPage/mainPage.dart
@@ -6,9 +6,11 @@ import 'Components/selectGymLabel.dart';
 
 class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
+
 }
 
 class _MainPageState  extends State<MainPage> {
+  final children = <Widget>[];
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
@@ -39,41 +41,34 @@ class _MainPageState  extends State<MainPage> {
            )
          ),
          Center(
-             child: CardRenderHourBased()
+             child: Column(children: CardRenderHourBased())
          )
        ]
      )
    );
   }
-   CardRenderHourBased() {
+   List<Widget>CardRenderHourBased() {
     var now = DateTime.now().hour;
     var twoHoursAfter = 2;
 
     if(now <= 4) { //Before 6h morning
       for(int hourToPutInCard = 6; hourToPutInCard <= 23; hourToPutInCard++ ){
-        // CardComponent(hour: hourToPutInCard, dateTime: new DateFormat("MMMMd","pt_BR").format(new DateTime.now()));
-         column:CardComponent(hour: hourToPutInCard, dateTime: new DateTime.now());
+         this.children.add(new CardComponent(hour: hourToPutInCard, dateTime: new DateTime.now()));
+
       }
     }
     else if(now >= 22){// After 22h
       DateTime nextDay = new DateTime.now();
-      // String nextDay = (new DateTime.now().day + 1).toString();
-      // String month = (new DateTime.now().month).toString();
-      // DateTime dateInformation = new DateFormat("MMMMd","pt_BR").parse(month + nextDay);
-      // DateTime dateInformation = nextDay.add(const Duration(days: 1));
-
       for(int hourToPutInCard = 6; hourToPutInCard <= 23; hourToPutInCard++) {
-        // CardComponent(hour: hourToPutInCard, dateTime: new DateFormat("MMMMd","pt_BR").format(new DateTime.now()));
-        builder: CardComponent(hour: hourToPutInCard, dateTime: nextDay.add(const Duration(days: 1)));
+        this.children.add(new CardComponent(hour: hourToPutInCard, dateTime: nextDay.add(const Duration(days: 1))));
       }
     }
     else {
       int hourToPutInCard = now + twoHoursAfter; //Only appear cards two hours after the current hour
-      // ignore: unnecessary_statements
       for (hourToPutInCard; hourToPutInCard <= 23; hourToPutInCard++) { //During the day
-        // CardComponent(hour: hourToPutInCard, dateTime: new DateFormat("MMMMd","pt_BR").format(new DateTime.now()));
-        builder: CardComponent(hour: hourToPutInCard, dateTime: new DateTime.now());
+        this.children.add(new CardComponent(hour: hourToPutInCard, dateTime: new DateTime.now()));
       }
     }
+    return this.children;
   }
 }
